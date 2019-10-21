fa56af929b71ca2908c1c5727958a26da0747ddc
王帅 <906145382@qq.com>
2019-10-21 22:50:58 +0800

    改变文件夹名字

--- Assets/doMain.cs
+++ Assets/doMain.cs
@@ -6,8 +6,8 @@ public class doMain : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        string inPath = @"C:\Users\BYWS\Desktop\1";
-        string outPath = @"C:\Users\BYWS\Desktop\out";
+        string inPath = @"C:\Users\BYWS\Desktop\vipIn";
+        string outPath = @"C:\Users\BYWS\Desktop\vipOut";
         new QQMusicVipDecode().Run(inPath,outPath);
 
         
